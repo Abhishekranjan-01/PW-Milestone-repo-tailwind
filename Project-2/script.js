3b38dfcd3e08c4cdd98ec898dafda3b934b9acdb
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2023-07-25 16:26:34 +0530

    Hero Section Completed With Responsiveness

---
+++ Project-2/script.js
@@ -0,0 +1,17 @@
+// Functionality for Hamburger
+const nav_links = document.querySelector('[data-nav_links]');
+        
+    
+function Menu(e){
+ e.name === 'menu' ? (e.name = 'close',
+ nav_links.classList.add('opacity-100'),
+ nav_links.classList.add('flex'),
+ nav_links.classList.remove('hidden'))
+ :
+ (e.name = 'menu',
+ nav_links.classList.add('hidden'),
+ nav_links.classList.remove('flex'),
+ nav_links.classList.remove('opacity-100')
+ );
+
+}
